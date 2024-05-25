bd67d3a2ca1b1f0bf8422522801c5258395550e1
aminebdiri <bdiri.amine08@gmail.com>
2024-05-25 15:25:26 +0100

    add config

---
+++ Exercice 2 du point de contrôle CSS Tailwind/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        '16': 'repeat(16, minmax(0, 1fr))',
+
+        // Complex site-specific column configuration
+        'footer': '200px minmax(900px, 1fr) 100px',
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/meme.png')",
+    },
+  },
+  plugins: [],
+}
+}
