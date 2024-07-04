892317f348c9f0326850b6ae57a9bd26109a9e9e
aminebdiri <bdiri.amine08@gmail.com>
2024-07-04 19:55:23 +0100

    remove files node

--- node/im.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const fs = require('fs')
-http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    req.pipe(fs.createWriteStream('./pic.png'));
-    res.end('success');
-  }).listen(8877);
